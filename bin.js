c39872e977e6bdc3a9fa44b1e9f498cdaee93f31
Lukas Horak <horaklukas@centrum.cz>
2019-03-12 20:48:42 +0100

    Add commander into bin and command for generating new changelog

--- bin.js
+++ bin.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-const fs = require('.');
+const program = require('commander');
+let cmdValue;
+
+program.version('0.2.1');
+
+program.command('new').action(function(cmd) {
+  cmdValue = cmd;
+
+  require('@babel/polyfill');
+  const run = require('auto-changelog/lib/run').default;
+  const argv = ['node', 'auto-changelog', '--template', 'keepachangelog', '-l', '20', '-b', '20'];
+
+  run(argv).catch(function(error) {
+    console.error(error);
+    process.exit(1);
+  });
+});
+
+program.parse(process.argv);
+
+if (!cmdValue) {
+  console.log('Generating record');
+  require('.');
+}
