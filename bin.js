0ea5cbaaa19b54f9d9448b10940ca62b254115ac
Lukas Horak <horaklukas@centrum.cz>
2019-03-12 21:01:28 +0100

    Add custom template for generating new changelog

--- bin.js
+++ bin.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const program = require('commander');
+const version = require('./package.json').version;
 let cmdValue;
 
-program.version('0.2.1');
+program.version(version);
 
 program.command('new').action(function(cmd) {
   cmdValue = cmd;
 
   require('@babel/polyfill');
   const run = require('auto-changelog/lib/run').default;
-  const argv = ['node', 'auto-changelog', '--template', 'keepachangelog', '-l', '20', '-b', '20'];
+  const argv = [
+    'node',
+    'auto-changelog',
+    '--template',
+    path.join(__dirname, './templates/custom-keepachangelog.hbs'),
+    '-l',
+    '20',
+    '-b',
+    '20',
+  ];
 
   run(argv).catch(function(error) {
     console.error(error);
